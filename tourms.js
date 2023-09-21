0b0d51dcb71b242e00fbf0b2d79ded9a7a614d66
Mulendra77 <139909764+Mulendra77@users.noreply.github.com>
2023-09-21 08:54:00 +0530

    Add files via upload

---
+++ tourms.js
@@ -0,0 +1,27 @@
+
+let formbtn = document.querySelector('#login-btn');
+let loginform = document.querySelector('.login-form-container');
+let formclose = document.querySelector('#form-close55432    ');
+
+
+window.onscroll = ()=>{
+    searchbtn.classList.remove('fa-time');
+    searchbtn.classList.remove('active');
+
+}
+searchbtn.addEventListener('click',()=>){
+    searchbtn.classList.toggle('fa-time');
+    searchbtn.classList.toggle('active');
+
+}
+searchbtn.addEventListener('click',()=>){
+    searchbtn.classList.toggle('fa-time');
+    searchbtn.classList.toggle('active');
+
+}
+searchbtn.addEventListener('click',()=>){
+    searchbtn.classList.toggle('fa-time');
+    searchbtn.classList.toggle('active');
+
+}
+
